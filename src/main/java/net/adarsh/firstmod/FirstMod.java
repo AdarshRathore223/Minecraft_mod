75ab7d56a9f1820a4bbb1b99aad7cd41e2a3341e
Adarsh Rathore <adarsh.rathore@s.amity.edu>
2023-11-25 19:31:35 +0530

    texture not working

--- src/main/java/net/adarsh/firstmod/FirstMod.java
+++ src/main/java/net/adarsh/firstmod/FirstMod.java
@@ -1,8 +1,11 @@
 package net.adarsh.firstmod;
 
 import com.mojang.logging.LogUtils;
+import net.adarsh.firstmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -24,15 +27,22 @@ public class FirstMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
-
+        modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event){
+        if(event.getTabKey()== CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.ZIRCON);
+        }
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
